2a4fdc447aa9927ba2e9a2252f2946879379ba75
Etson Dorival <35821623+etsond@users.noreply.github.com>
2024-01-31 16:51:23 +0000

    brushing java syntaxt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World! It's me Kathryn!");
+public class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("Hello World it's Etson");
+
 	}
+
 }
