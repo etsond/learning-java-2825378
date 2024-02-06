832d5689c5e471cd74620d42bdab78e3bd5561d8
Etson <etsond@ufl.edu>
2024-02-05 20:57:33 -0500

    using Scanner and nextDouble

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
+        System.out.println(studentFirstName +" "+ studentLastName + " has a GPA of "+ studentGPA);
+        System.out.println("what name Do you want of the student");
+
+        // print old gpa
+        System.out.println(studentGPA);
+        // create the input type scanner
+        Scanner input = new Scanner(System.in);
+        // find the next double of the scanner
+        studentGPA = input.nextDouble();
+        // print out the updated name of the input
+        System.out.println(studentGPA);
+
+        System.out.println(studentFirstName + " " + studentLastName + " now has gpa of " + studentGPA);
     }
 }
